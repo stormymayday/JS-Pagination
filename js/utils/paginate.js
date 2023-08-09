f3463487ae218c8ec98df3d8eac6a0ff2e0896d4
stormymayday <stormymayday7@gmail.com>
2023-08-09 11:47:59 -0700

    Creating array of arrays and then returning it

--- js/utils/paginate.js
+++ js/utils/paginate.js
@@ -5,6 +5,17 @@ const paginate = (followers) => {
     // Calculating the number of pages
     const numberOfPages = Math.ceil(followers.length / itemsPerPage);
 
+    // Creating array of arrays
+    const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+
+        const start = index * itemsPerPage;
+
+        return followers.slice(start, start + itemsPerPage);
+
+    });
+
+    return newFollowers;
+
 };
 
 export default paginate;
