2d63270e6dbc7d2f962b67a5a358b1a9c94b2f50
stormymayday <stormymayday7@gmail.com>
2023-08-08 20:31:26 -0700

    Returning article with the class of '.card'

--- js/displayFollowers.js
+++ js/displayFollowers.js
@@ -1,4 +1,4 @@
-import getElement from "./utils/getElement.js";
+aimport getElement from "./utils/getElement.js";
 
 const container = getElement('.container');
 
@@ -9,6 +9,12 @@ const displayFollowers = (followers) => {
     const newFollowers = followers.map((follower) => {
 
         const { avatar_url, login, html_url } = followers;
+
+        return `<article class="card">
+        <img src="${avatar_url}" alt="${login}">
+        <h4>${login}</h4>
+        <a href="${html_url} class="btn" target="_blank">view profile</a>
+        </article>`
 
     });
 
