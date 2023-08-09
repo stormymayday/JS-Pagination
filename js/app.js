27d57dc424efc6a58e8e14a54ba9fc784da616ba
stormymayday <stormymayday7@gmail.com>
2023-08-08 20:17:42 -0700

    Selecting the h1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 import fetchFollowers from "./fetchFollowers.js";
 import getElement from "../js/utils/getElement.js";
 
+const title = getElement('.section-title h1');
+console.log(title);
+
 const init = async () => {
 
     const followers = await fetchFollowers();
