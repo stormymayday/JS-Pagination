de093dd87c7deba68c0b5b2a65f47528cc2a9ece
stormymayday <stormymayday7@gmail.com>
2023-08-10 20:33:38 -0700

    Mapping over the pages array and returning button element assigning result to the btns variable

--- js/displayButtons.js
+++ js/displayButtons.js
@@ -1,6 +1,12 @@
 const displayButtons = (container, pages, activeIndex) => {
 
-    console.log(container, pages, activeIndex);
+    // console.log(container, pages, activeIndex);
+
+    let btns = pages.map((_, pageIndex) => {
+
+        return `<button class="page-btn">${pageIndex + 1}</button>`;
+
+    });
 
 };
 
