d0b700941add46761e65f26db3a555eae91e1857
stormymayday <stormymayday7@gmail.com>
2023-08-10 20:27:54 -0700

    Importing displayButtons in app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import getElement from "../js/utils/getElement.js";
 import fetchFollowers from "./fetchFollowers.js";
 import displayFollowers from "./displayFollowers.js";
+import displayButtons from "./displayButtons.js";
 import paginate from "./utils/paginate.js";
 
 const title = getElement('.section-title h1');
