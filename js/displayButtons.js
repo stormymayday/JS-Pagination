2985aa5ab5a8248087f613d9ee8a254f04da5493
stormymayday <stormymayday7@gmail.com>
2023-08-11 11:15:28 -0700

    Added the data-index attribute with pageIndex as a value to the buttons

--- js/displayButtons.js
+++ js/displayButtons.js
@@ -4,7 +4,7 @@ const displayButtons = (container, pages, activeIndex) => {
 
     let btns = pages.map((_, pageIndex) => {
 
-        return `<button class="page-btn ${activeIndex === pageIndex ? 'active-btn' : null}">${pageIndex + 1}</button>`;
+        return `<button class="page-btn ${activeIndex === pageIndex ? 'active-btn' : null}" data-index="${pageIndex}">${pageIndex + 1}</button>`;
 
     }).join('');
 
