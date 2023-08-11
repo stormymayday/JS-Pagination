feeffc16b19e46a04e195a0b99f9a3035bf8b375
stormymayday <stormymayday7@gmail.com>
2023-08-11 11:25:21 -0700

    Added the 'next' button

--- js/displayButtons.js
+++ js/displayButtons.js
@@ -4,9 +4,12 @@ const displayButtons = (container, pages, activeIndex) => {
 
     let btns = pages.map((_, pageIndex) => {
 
-        return `<button class="page-btn ${activeIndex === pageIndex ? 'active-btn' : null}" data-index="${pageIndex}">${pageIndex + 1}</button>`;
+        return `<button class="page-btn ${activeIndex === pageIndex ? 'active-btn' : 'null'}" data-index="${pageIndex}">${pageIndex + 1}</button>`;
 
     });
+
+    // Next Button
+    btns.push(`<button class="next-btn">next</button>`);
 
     container.innerHTML = btns.join('');
 
